682242e66b02c7d920df69fa644c667647e73fd9
Topai-Ioan <allexandru9801@gmail.com>
2022-05-01 13:04:08 +0300

    1 mai- adaugare skilluri

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,9 +28,26 @@ function linkAction() {
     //* when we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n=>n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
